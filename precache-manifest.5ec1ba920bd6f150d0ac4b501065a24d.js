f1012776a6806269bba9321b5779a2ce13c6b1f8
Justin Hove <hovej88@gmail.com>
2020-08-06 17:32:22 -0500

    Updates

---
+++ precache-manifest.5ec1ba920bd6f150d0ac4b501065a24d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd77bbe31b3b794a44c71c1bf89a61ec",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "9102ce41c64932ab5fd5",
+    "url": "/portfolio/static/css/main.81b8b80c.chunk.css"
+  },
+  {
+    "revision": "f2d6b8232ede20f2a2da",
+    "url": "/portfolio/static/js/2.fca23918.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.fca23918.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9102ce41c64932ab5fd5",
+    "url": "/portfolio/static/js/main.f591135e.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "696d8fe946d950ec99748a5231d6430f",
+    "url": "/portfolio/static/media/FacebookLogo.696d8fe9.png"
+  },
+  {
+    "revision": "51d17a05898314467e381245b30ed274",
+    "url": "/portfolio/static/media/GithubLogo.51d17a05.png"
+  },
+  {
+    "revision": "3e24dd5a9681c642b557381acab05263",
+    "url": "/portfolio/static/media/LinkedinLogo.3e24dd5a.png"
+  },
+  {
+    "revision": "93e46057e684777bc92f39ac5d463e8d",
+    "url": "/portfolio/static/media/Profile.93e46057.png"
+  }
+]);
